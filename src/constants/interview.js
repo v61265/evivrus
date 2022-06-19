1163611df50bd1ad5b267de6e7bdc66e1e1f3d94
YiHsuan <v61265@gmail.com>
2022-06-19 20:26:15 +0800

    feat: clean create-react-app template and create wording template of interview

---
+++ src/constants/interview.js
@@ -0,0 +1,109 @@
+export const wording = {
+  resume: {
+    experience: [
+      [{ style: "unstyled", content: "Google 工程師，東京，2019 - 2021" }],
+      [
+        {
+          style: "unstyled",
+          content: "Google 工程師，加州 Mountain View，2013 - 2015",
+        },
+      ],
+      [{ style: "unstyled", content: "Google 工程師，台北，2015 - 2019" }],
+      [{ style: "unstyled", content: "在 Google 內執行過 50 場以上的面試" }],
+    ],
+    education: [
+      [
+        {
+          style: "unstyled",
+          content: "Carnegie Mellon University 軟體工程碩士",
+        },
+      ],
+      [
+        {
+          style: "unstyled",
+          content: "台大資工系學士（b95）",
+        },
+      ],
+    ],
+    grade: [
+      [
+        {
+          style: "unstyled",
+          content: "台大資工系畢業名次 Top 5%，書卷獎三次",
+        },
+      ],
+    ],
+  },
+  students: [
+    {
+      name: "",
+      background: "",
+      icon: "",
+      contnet1: [],
+      content2: [],
+    },
+  ],
+  features: [
+    {
+      title: "Leetcode 解題指導與面試導向演算法教學",
+      content: [
+        {
+          title: "涵蓋面試常考的 12 大主題",
+          description: [
+            {
+              style: "unstyled",
+              content:
+                "將 Leetcode 題目分門別類，整理出 12 大常考主題，地毯式一ㄧ攻克",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  plans: [
+    {
+      title: ["Leetcode 解題指導", "面試導向演算法教學"],
+      contentList: [
+        [
+          {
+            style: "unstyled",
+            content: "包含 12 大常考主題，共 12 堂課，一堂課兩小時",
+          },
+          {
+            style: "unstyled",
+            content:
+              "若有預算或時間上的限制，可以可以縮減總時數，只涵蓋最常考的 top N 個演算法主題。例如，預算只能購買六小時（三堂課），那就來學最常考的前三大主題",
+          },
+        ],
+      ],
+    },
+  ],
+  charging: "收費方式｜一小時 2500 元",
+  method: [
+    {
+      title: "Leetcode 解題指導與面試導向演算法教學",
+      steps: [
+        [
+          {
+            style: "unstyled",
+            content:
+              "聯絡我們，安排 30 分鐘免費試聽，讓老師了解您目前的程度和需求，也讓您了解老師的教學方式",
+          },
+        ],
+      ],
+    },
+  ],
+  qaList: [
+    {
+      question:
+        "我是自學的軟體工程師，沒有讀過資訊相關科系，也可以上這個課程嗎？",
+      answer: [
+        {
+          style: "unstyled",
+          content:
+            "面試導向演算法教學課程極度適合「自學程式」且「已經在工作」的工程師！對於演算法和資料結構，老師會針對您不會的地方從零教起，已經會的部分則可以跳過節省時間",
+        },
+      ],
+    },
+  ],
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+// import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const theme = {
+  color: {
+    primary: "#F9ADAD",
+    primaryLight: "#FAE0E0",
+    primaryDark: "#BB8080",
+    secondary: "#212121",
+    secondaryLight: "#48484848",
+    secondaryDark: "#212121",
+    white: "#FEFEFE",
+    black: "#000000",
+    grey: "#C4C4C4",
+    greyLight: "#E5E5E5",
+    greyDark: "#656565",
+    alert: "#FF0000",
+    background: "#F1F0F0",
+    backgroundIfLight: "#F56F6F",
+    backgroundIfDark: "#F05454",
+    mask: "#21212166",
+  },
+  font: {
+    xxs: 14,
+    xs: 16,
+    sm: 18,
+    md: 20,
+    lg: 22,
+    xl: 24,
+    xxl: 30,
+    xxxl: 40,
+  },
+  space: {
+    xxs: 4,
+    xs: 8,
+    sm: 10,
+    md: 30,
+    lg: 40,
+    xl: 60,
+    xxl: 80,
+    xxxxl: 120,
+  },
+  icon: {
+    sm: 24,
+    md: 36,
+  },
+  media: {
+    sm: "@media (max-width: 540px)",
+    md: "@media (max-width: 640px)",
+  },
+  shadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+};
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>
+  </ThemeProvider>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
